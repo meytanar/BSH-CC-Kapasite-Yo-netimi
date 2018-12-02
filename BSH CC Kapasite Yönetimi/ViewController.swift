b253cdfac9fb9626146da28593c3a208fc4ad280
BSH_MRM <bsh_mrm@bsh-mrm-macbook-pro.local>
2018-12-02 16:06:04 +0300

    Initial Commit

---
+++ BSH CC Kapasite Yönetimi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BSH CC Kapasite Yönetimi
+//
+//  Created by BSH_MRM on 2.12.2018.
+//  Copyright © 2018 BSH_MRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
